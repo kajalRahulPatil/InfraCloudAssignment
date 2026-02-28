0bfe005947b6be5e94bcd2c3265b5aeb2319bd38
KajalRahulPatil <kajal2693@gmail.com>
2026-02-28 14:36:49 +0530

    base62 encoder

---
+++ urlshortener/src/main/java/com/example/urlshortener/UrlService.java
@@ -0,0 +1,16 @@
+package com.example.urlshortener;
+
+public class UrlService {
+    
+    private static final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static String encode(long num) {
+        StringBuilder sb = new StringBuilder();
+
+        while (num > 0) {
+            sb.append(BASE62.charAt((int) (num % 62)));
+            num /= 62;
+        }
+        return sb.reverse().toString();
+    }
+}
